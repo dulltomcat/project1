a468b3abd7d74751442cd626846beea984c14171
dulltomcat <codevault@yandex.ru>
2019-01-02 17:39:27 +0300

    добавление модуля goods

--- app/Http/Controllers/CatController.php
+++ app/Http/Controllers/CatController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AbcController extends Controller
+class CatController extends Controller
 {
     public function index()
     {
-        return view ("pages.abc.index");
+        return view ("pages.cat.index");
     }
 }
--- resources/views/pages/cat/index.blade.php
+++ resources/views/pages/cat/index.blade.php
@@ -1,8 +1,6 @@
 @extends ("layouts.app")
 @section("content")
 
-    abc new view
+   cat
 
 @endsection
-
-
